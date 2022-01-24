3f74638266a4f9e4f243cd7c34d4f8429f274779
vedant-1234 <77705019+vedant-1234@users.noreply.github.com>
2022-01-23 21:27:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var wrongaudio = document.getElementById("no")
+
+function wrong() 
+{
+    wrongaudio.play();
+}
+
+var rightaudio = document.getElementById("yes")
+
+function right()
+{
+    rightaudio.play();
+}
